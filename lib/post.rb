c99065adb8e8a18a27e3e8d6c679ee1051117c85
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-27 13:45:25 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,6 @@
+class Post
+  attr_accessor :title, :author
+  end
+author = Author.new
+post = Post.new
+post.author = author
---
+++ lib/song.rb
@@ -0,0 +1,6 @@
+class Song
+  attr_accessor :title, :artist
+end
+artist = Artist.new
+song = Song.new
+song.artist = artist
